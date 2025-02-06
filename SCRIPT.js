6bbff9aa32cf43a65203487b99608e84dc520d2c
Whitecriminalxx <101876369+Whitecriminalxx@users.noreply.github.com>
2025-02-06 17:50:42 +0530

    first commit

---
+++ SCRIPT.js
@@ -0,0 +1,4 @@
+console.log('External javascript is working')
+document.getElementsByClassName("box")[0].style.backgroundColor = "red";
+document.getElementById("one").innerHTML="zuko";
+// document.getElementsByClassName("box")[1].style.backgroundColor="red";
