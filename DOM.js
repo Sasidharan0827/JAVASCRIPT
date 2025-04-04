b755b79073900040344106cb6db4800be52681cd
Sasi⚡Dharan <138274308+Sasidharan0827@users.noreply.github.com>
2025-04-04 10:33:38 +0530

    dom explanation

---
+++ DOM.js
@@ -0,0 +1,10 @@
+//DOM
+//Document object mode
+//document ------->stand for what type of document (html,xml)
+//object stands for the elements (h1,div)
+//model satnds for the layout structure like opening and closing tag
+//the <html> is the root element tag other elements are sub-elements
+// the document is object that contains the entire elements in it
+//document.getelementbyid.--> is used to make changes in the element
+//document.getelementbyclass--->it also works similar
+//by this we can intract with api
